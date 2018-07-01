de90e6c0b183fd2e3742baf93f23b157e631a808
mydeskmate <516622145@qq.com>
2018-07-01 14:38:15 +0800

    ddd

--- test.py
+++ test.py
@@ -4,22 +4,6 @@ user_status = False #用户登录了就把这个改成True
 def login(auth_type): #把要执行的模块从这里传进来
     def auth(func):
         def inner(*args,**kwargs):#再定义一层函数
-            if auth_type == "qq":
-                _username = "alex" #假装这是DB里存的用户信息
-                _password = "123456" #假装这是DB里存的用户信息
-                global user_status
-
-                if user_status == False:
-                    username = input("user:")
-                    password = input("pasword:")
-
-                    if username == _username and password == _password:
-                        print("welcome login....")
-                        user_status = True
-                    else:
-                        print("wrong username or password!")
-
-                if user_status == True:
                     return func(*args,**kwargs) # 看这里看这里，只要验证通过了，就调用相应功能
             else:
                 print("only support qq ")
