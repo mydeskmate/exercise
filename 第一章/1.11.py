c9957aaa623e896ed2a66f77523b1cdabf550fd4
mydeskmate <516622145@qq.com>
2018-12-11 16:50:19 +0800

    new

---
+++ 第一章/1.11.py
@@ -0,0 +1,13 @@
+age = 33
+count = 0
+while count < 3:
+    guess_age = int(input("Guess the age: "))
+    if guess_age == age:
+        print("You guess right")
+        break
+    count+=1
+    if count == 3:
+        conti_play = input("还有继续玩吗？（y/n）")
+        if conti_play == 'y':
+            count = 0
+
