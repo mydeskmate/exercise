795396eedfd57e00ddef8aa8658de07b640d7656
mydeskmate <516622145@qq.com>
2018-12-11 15:05:01 +0800

    new

--- test.py
+++ test.py
@@ -1,35 +1,12 @@
-#_*_coding:utf-8_*_
-user_status = False #用户登录了就把这个改成True
-
-def login(auth_type): #把要执行的模块从这里传进来
-    def auth(func):
-        def inner(*args,**kwargs):#再定义一层函数
-                    return func(*args,**kwargs) # 看这里看这里，只要验证通过了，就调用相应功能
-            else:
-                print("only support qq ")
-        return inner #用户调用login时，只会返回inner的内存地址，下次再调用时加上()才会执行inner函数
-
-    return auth
-
-def home():
-    print("---首页----")
-
-@login('qq')
-def america():
-    #login() #执行前加上验证
-    print("----欧美专区----")
-
-def japan():
-    print("----日韩专区----")
-
-@login('qq')
-def henan(style):
-    '''
-    :param style: 喜欢看什么类型的，就传进来
-    :return:
-    '''
-    #login() #执行前加上验证
-    print("----河南专区----")
-
-
-henan("33333")
+def maopao(lists):
+    count = len(lists)
+    while count > 0:
+        i = 0
+        while i < count-1:
+            if lists[i] > lists[i+1]:
+                print(lists)
+                lists[i],lists[i+1] = lists[i+1],lists[i]
+            i += 1
+        count -= 1
+    return lists
+print(maopao([4,3,2,8,7,5]))
