29866cd837e3b70ba57eb8bb8ed789d3e4d1286e
mydeskmate <516622145@qq.com>
2018-12-20 22:46:37 +0800

    new

--- test.py
+++ test.py
@@ -1,16 +1,9 @@
-import hashlib
-import time
-class MySQL:
-    def __init__(self,host,port):
-        self.id=self.create_id()
-        self.host=host
-        self.port=port
-    @staticmethod
-    def create_id(): #就是一个普通工具
-        m=hashlib.md5(str(time.time()).encode('utf-8'))
-        return m.hexdigest()
-
-
-print(MySQL.create_id) #<function MySQL.create_id at 0x0000000001E6B9D8> #查看结果为普通函数
-conn=MySQL('127.0.0.1',3306)
-print(conn.create_id) #<function MySQL.create_id at 0x00000000026FB9D8> #查看结果为普通函数
+s1 = 'hello'
+try:
+    int(s1)
+except IndexError as e:
+    print(e)
+except KeyError as e:
+    print(e)
+except ValueError as e:
+    print(e)
