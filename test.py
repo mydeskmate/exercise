054104fa508fa6444318f09d1f63714694bbb02a
mydeskmate <516622145@qq.com>
2018-12-21 10:02:38 +0800

    new

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import hashlib
 import time
 class MySQL:
+
     def __init__(self,host,port):
         self.id=self.create_id()
         self.host=host
@@ -14,3 +15,5 @@ class MySQL:
 print(MySQL.create_id) #<function MySQL.create_id at 0x0000000001E6B9D8> #查看结果为普通函数
 conn=MySQL('127.0.0.1',3306)
 print(conn.create_id) #<function MySQL.create_id at 0x00000000026FB9D8> #查看结果为普通函数
+
+
